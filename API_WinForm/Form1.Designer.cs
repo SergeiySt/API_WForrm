e691cf9c926ff0e80e72ebf5cecd9cd60c5d4260
SergeiySt <ser.st.1989@gmail.com>
2023-06-24 21:48:25 +0300

    add_1

---
+++ API_WinForm/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace API_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_Pictute = new System.Windows.Forms.PictureBox();
+            this.button_Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pictute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Pictute
+            // 
+            this.pictureBox_Pictute.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_Pictute.Name = "pictureBox_Pictute";
+            this.pictureBox_Pictute.Size = new System.Drawing.Size(776, 365);
+            this.pictureBox_Pictute.TabIndex = 0;
+            this.pictureBox_Pictute.TabStop = false;
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Exit.Location = new System.Drawing.Point(688, 399);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(100, 39);
+            this.button_Exit.TabIndex = 1;
+            this.button_Exit.Text = "Вихід";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.pictureBox_Pictute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pictute)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Pictute;
+        private System.Windows.Forms.Button button_Exit;
+    }
+}
+
---
+++ API_WinForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace API_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadImageFromAPIAsync();
+        }
+
+        private async Task LoadImageFromAPIAsync()
+        {
+
+            string imageUrl = "https://pixabay.com/api/?key=37619946-e0bf9bf12e55406f12d15ff99&q=yellow+flowers&image_type=photo";
+
+            try
+            {
+                WebClient client = new WebClient();
+                byte[] imageData = await client.DownloadDataTaskAsync(imageUrl);
+                using (MemoryStream ms = new MemoryStream(imageData))
+                {
+                    Image image = Image.FromStream(ms);
+                    pictureBox_Pictute.Image = image;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке изображения: " + ex.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();        
+        }  
+    }
+}
